7196e2e3b52251499b38ddfa0681468005f466cc
Rodrigo Paula Gama <rodrigo.pgama@outlook.com>
2025-02-24 09:49:55 -0300

    Refactor Organization and User models to remove OwnerId and Username properties from constructors

--- src/QFXTaskMan.Core/Models/Organization.cs
+++ src/QFXTaskMan.Core/Models/Organization.cs
@@ -9,19 +9,15 @@ public sealed class Organization : BaseClass
         
     }
 
-    public Organization(string name, string description, Guid ownerId)
+    public Organization(string name, string description)
     {
         Name = name;
         Description = description;
-        OwnerId = ownerId;
     }
 
     [Required(ErrorMessage = "{0} is required"), MaxLength(100, ErrorMessage = "{0} cannot be longer than 100 characters")]
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-
-    // Foreign keys
-    public required Guid OwnerId { get; set; }
 
     // Navigation properties
     public required User Owner { get; set; }
--- src/QFXTaskMan.Core/Models/User.cs
+++ src/QFXTaskMan.Core/Models/User.cs
@@ -9,11 +9,10 @@ public sealed class User : BaseClass
 
     }
 
-    public User(string name, string email, string username, string passwordHash)
+    public User(string name, string email, string passwordHash)
     {
         Name = name;
         Email = email;
-        Username = username;
         PasswordHash = passwordHash;
     }
 
@@ -22,9 +21,6 @@ public sealed class User : BaseClass
     
     [Required(ErrorMessage = "{0} is required"), MaxLength(256, ErrorMessage = "{0} cannot be longer than 256 characters")]
     public string Email { get; set; } = "";
-    
-    [Required(ErrorMessage = "{0} is required"), MaxLength(100, ErrorMessage = "{0} cannot be longer than 100 characters")]
-    public string Username { get; set; } = "";
     
     [Required(ErrorMessage = "{0} is required"), MinLength(8, ErrorMessage = "{0} must be at least 8 characters long"), RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "{0} must contain at least one uppercase letter, one lowercase letter, and one number")]
     public string PasswordHash { get; set; } = "";
